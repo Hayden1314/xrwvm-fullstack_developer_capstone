707e0c7a5044e6798f17cb68f44e0589733ff67e
hayden <haydenyau1314@gmail.com>
2024-11-13 10:49:26 -0500

    lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -146,13 +146,12 @@ def add_review(request):
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    print count
+    print(count)
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
         cars.append({'CarModel': car_model.name,
-                    'CarMake': car_model.car_make.name})
+                     'CarMake': car_model.car_make.name})
     return JsonResponse({'CarModels': cars})
-    
