33fbe32b8b2eac7320f370969a2e3194dee5c1da
hayden <haydenyau1314@gmail.com>
2024-11-13 11:12:15 -0500

    lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://haydenyau131-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://haydenyau131-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.'
+        'cognitiveclass.ai'
+    ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://haydenyau131-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://haydenyau131-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.'
+        'cognitiveclass.ai'
+    ),
 ]
 
 REST_FRAMEWORK = {
@@ -98,13 +104,19 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
+    },
 ]
-
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
